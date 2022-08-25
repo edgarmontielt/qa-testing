f645a394b5a36f52f52cb4f75c6043614b395993
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-25 12:21:38 -0500

    add: first test passed

--- func/tax.js
+++ func/tax.js
@@ -1,12 +1,17 @@
 // Tax -> 0-5000 5%, 5001-15000 10%, >15000 30%
 
 const calculateTax = (income) => {
-    if (0 < income <= 5000) {
+
+    if (income < 0) return 'No negative numbers'
+
+    if (income > 0 && income <= 5000) {
         return income * 0.05
-    } else if (5001 < income <= 15000) {
+    } else if (income > 5000 && income <= 15000) {
         return income * 0.15
-    } else if (15000<income) {
-        return income*0.3
+    } else if (15000 < income) {
+        return income * 0.3
+    } else {
+        return new Error('Error')
     }
 }
 
--- testing/tax.test.js
+++
@@ -1,4 +0,0 @@
-// Mocha: Suite of test -> estructura para armar pruebas
-// Chai: Asserts -> condiciones de prueba
-
-const chai = require('chai')
